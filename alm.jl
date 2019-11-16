401d2d4a0dcc479cad8495f5fc2133aad725b777
Michel Schanen <Michel.Schanen@gmail.com>
2019-11-15 18:34:27 -0600

    seems working

--- alm.jl
+++ alm.jl
@@ -52,19 +52,11 @@ function lagrangian(x::Vector{Float64},lambda::Vector{Float64},mu::Float64)
 end
 
 function normGradientLag(x::Vector{Float64},lambda::Vector{Float64},c::Float64)
-    # Evalue la fonction en xcurrent
     f,g,h = lagrangian(x, lambda, c)
-    # calcule de la norme du gradient au point courant
     valueNG = norm(g(x))
 end
 
 function newton(x::Vector{Float64},lambda::Vector{Float64},c::Float64, epsilon; verbose = true)
-    # Function which gives the steepest descent according to Newton's method
-    # INPUT: fonction = name of the file containing the function of interest
-    #        x = point at which we evaluate the 
-    #        lambda = dual variable at which we evaluate the Lagrangian
-    #        c = penalty value of the Lagrangian
-    # OUTPUT: d = steepest direction according to Newton's method
     f,g,h = lagrangian(x, lambda, c)
     res = 1e16
     iter = 0
@@ -82,23 +74,6 @@ function newton(x::Vector{Float64},lambda::Vector{Float64},c::Float64, epsilon; 
 end
 
 function solve(param;verbose = true)
-    #  Function implementing the augmented Lagrangian algorithm of the
-    #  Bierlaire's book.
-    #  INPUTS: - fct: string name of the file containing Lagrangians (function,
-    #  gradient, Hessian)
-    #         - pen: string name of the file containing constraints (function,
-    #  gradient, Hessian)
-    #         - param: structure containing the following fields
-    #                param.maxiter: max number of iterations
-    #                param.lambda, param.c, param.x: initial variables used to calculate the
-    #                Lagrangian
-    #                param.eta, param.tau, param.alpha, param.beta: Augmented
-    #                Lagrangian parameters (cf Bierlaire's book)
-    #                param.epsilon2: stopping criterion of the algorithm
-    #                param.linesearch: selected linesearch method
-    #                param.edit: display option
-    # OUTPUTS: - X: List containing the values of X along the iterations
-    #          - lambda: List containg the lambda at optimum
     # - Initialization
     maxiter = param.maxiter
     lambda = param.lambda
@@ -113,24 +88,14 @@ function solve(param;verbose = true)
     epsilon0 = epsilon
     eta0 = eta
     eta = eta/c^alpha
-    fpen = pen(x)
     k = 0
     # - Iterations
-    while (normGradientLag(x, lambda, c) > epsilon2 || norm(fpen)^2 > epsilon2) && k<maxiter
-        # - Linesearch
+    while norm(lagrangian(x, lambda, c)[2](x)) > epsilon2 && k<maxiter
+        # - Newton 
         x = newton(x, lambda, c, epsilon, verbose=verbose)
-        fpen = pen(x)
         
         # - Updating dual variables and precision of the linesearch
-        # if (norm(fpen) <= eta)
-            lambda = lambda - c*fpen
-            # c = c*1.2
-            # epsilon = epsilon/c
-            # eta = eta/c^beta
-        # else
-            # epsilon = epsilon0/c
-            # eta = eta0/c^alpha
-        # end
+        lambda = update_lambda(lambda, c, x)
         k = k+1
         
         # - Display of the results at each iteration
@@ -139,11 +104,10 @@ function solve(param;verbose = true)
             println("Iteration: ", k)
             println("c: ", c)
             println("epsilon: ", epsilon)
-            println("pen = ", fpen)
             println("lambda = ", lambda)
             println("x= ", x)
             println("Value of the Lagrangian Gradient norm: ", normGradientLag(x, lambda, c))
-            println("Value of the Constraint norm: ", norm(fpen))
+            println("Value of the Constraint norm: ", violations(x))
             println("objective: ", x[1] * x[4] * (x[1] + x[2] + x[3]) + x[3] )
         end
     end
@@ -152,31 +116,65 @@ function solve(param;verbose = true)
         println("Iteration: ", k)
         println("c: ", c)
         println("epsilon: ", epsilon)
-        println("pen = ", fpen)
         println("lambda = ", lambda)
         println("x= ", x)
         println("Value of the Lagrangian Gradient norm: ", normGradientLag(x, lambda, c))
-        println("Value of the Constraint norm: ", norm(fpen))
+        println("Value of the Constraint norm: ", violations(x))
         println("objective: ", x[1] * x[4] * (x[1] + x[2] + x[3]) + x[3] )
     end
     x, lambda
 end
 
-function pen(x::Vector{Float64})
+function update_lambda(lambda::Vector{Float64}, mu::Float64, x::Vector{Float64})
 # h is a vector containing the value of the different penalty functions for
 # a given x
     h = Array{Float64,1}(undef, 10)
+    # Equalities
     h[1] = sum(el^2 for el in x) - 40
-    h[2] = (prod(el for el in x) - 25)
-    h[3] = x[1] - 1
-    h[4] = x[2] - 1
-    h[5] = x[3] - 1
-    h[6] = x[4] - 1
-    h[7] = 5 - x[1]
-    h[8] = 5 - x[2]
-    h[9] = 5 - x[3]
-    h[10] = 5 - x[4]
+    # Inequalities
+    h[2] = update_lambda(lambda[2], mu, (prod(el for el in x) - 25))
+    h[3] = update_lambda(lambda[3], mu, x[1] - 1)
+    h[4] = update_lambda(lambda[4], mu, x[2] - 1)
+    h[5] = update_lambda(lambda[5], mu, x[3] - 1)
+    h[6] = update_lambda(lambda[6], mu, x[4] - 1)
+    h[7] = update_lambda(lambda[7], mu, 5 - x[1])
+    h[8] = update_lambda(lambda[8], mu, 5 - x[2])
+    h[9] = update_lambda(lambda[9], mu, 5 - x[3])
+    h[10] = update_lambda(lambda[10], mu, 5 - x[4])
     h
+end
+
+function ineq_violation(f)
+    if f < 0.0
+        return f
+    else
+        return 0.0
+    end
+end
+
+
+function violations(x::Vector{Float64})
+    c = Array{Float64}(undef, 10)
+    c[1] = sum(el^2 for el in x) - 40
+    # Inequalities
+    c[2] = ineq_violation(prod(el for el in x) - 25)
+    c[3] = ineq_violation(x[1] - 1)
+    c[4] = ineq_violation(x[2] - 1)
+    c[5] = ineq_violation(x[3] - 1)
+    c[6] = ineq_violation(x[4] - 1)
+    c[7] =  ineq_violation(5 - x[1])
+    c[8] =  ineq_violation(5 - x[2])
+    c[9] = ineq_violation( 5 - x[3])
+    c[10] =  ineq_violation(5 - x[4])
+    c
+end
+
+function update_lambda(lambda::Float64, mu::Float64, c::Float64)
+    if -c + lambda/mu <= 0 
+        return 0
+    else
+        return lambda - mu*c
+    end
 end
 
 mutable struct Params
@@ -202,11 +200,12 @@ using .alm
 #  on a typical example
 # - Parameters for test
 param = alm.Params()
-param.maxiter = 100# maximum ALM iterations
+param.maxiter = 1000# maximum ALM iterations
 param.lambda = ones(Float64,10)
-param.c = 1000
+param.c = 10
 param.eta = 0.1 * 10.0^(0.1)
 param.x0 = [1.000, 4.743, 3.821, 1.379]
+# param.x0 = [1.000, 5.0, 5.0, 1.0]
 param.tau = 100
 param.alpha = 0.1
 param.beta = 0.9
@@ -215,4 +214,4 @@ param.epsilon2 = 0.5
 x0 = param.x0
 println("lagrangian: ", alm.lagrangian(x0, zeros(Float64,10), 10000.0)[1](x0))
 println("objective: ", x0[1] * x0[4] * (x0[1] + x0[2] + x0[3]) + x0[3] )
-x, lambda = alm.solve(param; verbose = true)
+x, lambda = alm.solve(param; verbose = false)
